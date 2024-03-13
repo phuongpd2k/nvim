d701dcdc27c0f0fdd2d68da28c21ebef4244d078
phuongpd <phuongpd.work@gmail.com>
2024-03-13 08:47:12 +0700

    Init Project

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,57 @@
+local keymap = vim.keymap
+local opts = { noremap = true, silent = true }
+
+keymap.set("n", "x", '"_x')
+
+-- Increment/decrement
+keymap.set("n", "+", "<C-a>")
+keymap.set("n", "-", "<C-x>")
+
+-- Select all
+keymap.set("n", "<C-a>", "gg<S-v>G")
+
+-- Save file and quit
+keymap.set("n", "<Leader>w", ":update<Return>", opts)
+keymap.set("n", "<Leader>q", ":quit<Return>", opts)
+keymap.set("n", "<Leader>Q", ":qa<Return>", opts)
+
+-- File explorer with NvimTree
+keymap.set("n", "<Leader>f", ":NvimTreeFindFile<Return>", opts)
+keymap.set("n", "<Leader>t", ":NvimTreeToggle<Return>", opts)
+
+-- Tabs
+keymap.set("n", "te", ":tabedit")
+keymap.set("n", "<tab>", ":tabnext<Return>", opts)
+keymap.set("n", "<s-tab>", ":tabprev<Return>", opts)
+keymap.set("n", "tw", ":tabclose<Return>", opts)
+
+-- Split window
+keymap.set("n", "ss", ":split<Return>", opts)
+keymap.set("n", "sv", ":vsplit<Return>", opts)
+
+-- Move window
+keymap.set("n", "sh", "<C-w>h")
+keymap.set("n", "sk", "<C-w>k")
+keymap.set("n", "sj", "<C-w>j")
+keymap.set("n", "sl", "<C-w>l")
+
+-- Resize window
+keymap.set("n", "<C-S-h>", "<C-w><")
+keymap.set("n", "<C-S-l>", "<C-w>>")
+keymap.set("n", "<C-S-k>", "<C-w>+")
+keymap.set("n", "<C-S-j>", "<C-w>-")
+
+-- Diagnostics
+-- keymap.set("n", "<C-j>", function()
+-- 	vim.diagnostic.goto_next()
+-- end, opts)
+
+-- Custom Move
+keymap.set("n", "j", "jzz")
+keymap.set("n", "k", "kzz")
+keymap.set("n", "J", "5jzz")
+keymap.set("n", "K", "5kzz")
+keymap.set("v", "j", "jzz")
+keymap.set("v", "k", "kzz")
+keymap.set("v", "J", "5jzz")
+keymap.set("v", "K", "5kzz")
---
+++ lua/plugins/ui.lua
@@ -0,0 +1,284 @@
+return {
+	{
+		"nvimdev/dashboard-nvim",
+		enabled = false,
+	},
+	{
+		"nvim-lualine/lualine.nvim",
+		enabled = true,
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		opts = function(_, opts)
+			local function show_macro_recording()
+				local recording_register = vim.fn.reg_recording()
+				if recording_register == "" then
+					return ""
+				else
+					return "Recording @" .. recording_register
+				end
+			end
+
+			vim.api.nvim_create_autocmd("RecordingEnter", {
+				callback = function()
+					require("lualine").refresh({
+						place = { "statusline" },
+					})
+				end,
+			})
+
+			vim.api.nvim_create_autocmd("RecordingLeave", {
+				callback = function()
+					local timer = vim.loop.new_timer()
+					timer:start(
+						50,
+						0,
+						vim.schedule_wrap(function()
+							require("lualine").refresh({
+								place = { "statusline" },
+							})
+						end)
+					)
+				end,
+			})
+
+			opts.options = {
+				icons_enabled = true,
+				theme = "auto",
+				component_separators = { left = "", right = "" },
+				section_separators = { left = "", right = "" },
+				disabled_filetypes = {
+					inactive_winbar = {},
+					statusline = { "alpha", "dashboard", "fzf", "lazy", "mason", "TelescopePrompt" },
+					tabline = {},
+					winbar = {},
+				},
+				ignore_focus = {},
+				always_divide_middle = true,
+				globalstatus = true,
+				refresh = {
+					statusline = 1000,
+					tabline = 1000,
+					winbar = 1000,
+				},
+			}
+
+			opts.sections = {
+				lualine_a = { "mode" },
+				lualine_b = { "branch", "diff", "diagnostics" },
+				lualine_c = { "filename", "open_file" },
+				lualine_x = {
+					{ show_macro_recording },
+					{ "progress", separator = " ", padding = { left = 1, right = 0 } },
+					{ "location", padding = { left = 0, right = 1 } },
+				},
+				lualine_y = { "fileformat", "filetype" },
+				lualine_z = { "encoding" },
+			}
+
+			opts.tabline = {}
+			opts.winbar = {}
+			opts.inactive_winbar = {}
+			opts.extensions = { "neo-tree", "lazy" }
+		end,
+	},
+	-- messages, cmdline and the popupmenu
+	{
+		"folke/noice.nvim",
+		opts = function(_, opts)
+			table.insert(opts.routes, {
+				filter = {
+					event = "notify",
+					find = "No information available",
+				},
+				opts = { skip = true },
+			})
+			local focused = true
+			vim.api.nvim_create_autocmd("FocusGained", {
+				callback = function()
+					focused = true
+				end,
+			})
+			vim.api.nvim_create_autocmd("FocusLost", {
+				callback = function()
+					focused = false
+				end,
+			})
+			table.insert(opts.routes, 1, {
+				filter = {
+					cond = function()
+						return not focused
+					end,
+				},
+				view = "notify_send",
+				opts = { stop = false },
+			})
+
+			opts.commands = {
+				all = {
+					-- options for the message history that you get with `:Noice`
+					view = "split",
+					opts = { enter = true, format = "details" },
+					filter = {},
+				},
+			}
+
+			opts.presets.lsp_doc_border = true
+		end,
+	},
+
+	{
+		"rcarriga/nvim-notify",
+		opts = {
+			timeout = 5000,
+			background_colour = "#000000",
+			render = "wrapped-compact",
+		},
+	},
+
+	-- buffer line
+	{
+		"akinsho/bufferline.nvim",
+		event = "VeryLazy",
+		keys = {
+			{ "<Tab>", "<Cmd>BufferLineCycleNext<CR>", desc = "Next tab" },
+			{ "<S-Tab>", "<Cmd>BufferLineCyclePrev<CR>", desc = "Prev tab" },
+		},
+		opts = {
+			options = {
+				mode = "tabs",
+				show_buffer_close_icons = false,
+				show_close_icon = false,
+			},
+		},
+	},
+
+	-- filename
+	{
+		"b0o/incline.nvim",
+		dependencies = {},
+		event = "BufReadPre",
+		priority = 1200,
+		config = function()
+			local helpers = require("incline.helpers")
+			require("incline").setup({
+				window = {
+					padding = 0,
+					margin = { horizontal = 0 },
+				},
+				render = function(props)
+					local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
+					local ft_icon, ft_color = require("nvim-web-devicons").get_icon_color(filename)
+					local modified = vim.bo[props.buf].modified
+					local buffer = {
+						-- ft_icon and { " ", ft_icon, " ", guibg = ft_color, guifg = helpers.contrast_color(ft_color) }
+						-- 	or "",
+						-- " ",
+						{ filename, gui = modified and "bold,italic" or "bold" },
+						" ",
+						-- guibg = "#363944",
+					}
+					return buffer
+				end,
+			})
+		end,
+	},
+	-- LazyGit integration with Telescope
+	{
+		"kdheepak/lazygit.nvim",
+		keys = {
+			{
+				";c",
+				":LazyGit<Return>",
+				silent = true,
+				noremap = true,
+			},
+		},
+		-- optional for floating window border decoration
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+	},
+	{
+		"kristijanhusak/vim-dadbod-ui",
+		dependencies = {
+			{ "tpope/vim-dadbod", lazy = true },
+			{ "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true },
+		},
+		cmd = {
+			"DBUI",
+			"DBUIToggle",
+			"DBUIAddConnection",
+			"DBUIFindBuffer",
+		},
+		init = function()
+			-- Your DBUI configuration
+			vim.g.db_ui_use_nerd_fonts = 1
+		end,
+		keys = {
+			{
+				"<leader>d",
+				"<cmd>NvimTreeClose<cr><cmd>tabnew<cr><bar><bar><cmd>DBUI<cr>",
+			},
+		},
+	},
+	{
+		"nvim-tree/nvim-tree.lua",
+		config = function()
+			require("nvim-tree").setup({
+				on_attach = function(bufnr)
+					local api = require("nvim-tree.api")
+
+					local function opts(desc)
+						return {
+							desc = "nvim-tree: " .. desc,
+							buffer = bufnr,
+							noremap = true,
+							silent = true,
+							nowait = true,
+						}
+					end
+
+					-- default mappings
+					api.config.mappings.default_on_attach(bufnr)
+
+					-- custom mappings
+					vim.keymap.set("n", "t", api.node.open.tab, opts("Tab"))
+				end,
+				actions = {
+					open_file = {
+						quit_on_open = true,
+					},
+				},
+				sort = {
+					sorter = "case_sensitive",
+				},
+				view = {
+					width = 30,
+					relativenumber = true,
+				},
+				renderer = {
+					group_empty = true,
+				},
+				filters = {
+					dotfiles = true,
+					custom = {
+						"node_modules/.*",
+					},
+				},
+				log = {
+					enable = true,
+					truncate = true,
+					types = {
+						diagnostics = true,
+						git = true,
+						profile = true,
+						watcher = true,
+					},
+				},
+			})
+
+			if vim.fn.argc(-1) == 0 then
+				vim.cmd("NvimTreeFocus")
+			end
+		end,
+	},
+}
